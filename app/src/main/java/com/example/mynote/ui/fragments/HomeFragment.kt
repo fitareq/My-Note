487955f68b37b306dfe54fbf4a48cd2e710cace8
Forhadul Islam <fitareq@gmail.com>
2022-08-02 19:08:55 +0600

    bottom sheet

--- app/src/main/java/com/example/mynote/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/mynote/ui/fragments/HomeFragment.kt
@@ -27,7 +27,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
 
